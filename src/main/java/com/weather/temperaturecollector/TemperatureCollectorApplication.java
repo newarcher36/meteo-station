c4cc0c54b6edde2094d3069e7b106eed004cd6e2
danijm26 <danijm26@gmail.com>
2022-02-15 09:28:33 +0100

    Inital commit

---
+++ src/main/java/com/weather/temperaturecollector/TemperatureCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.weather.temperaturecollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/temperaturecollector/TemperatureCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.temperaturecollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
