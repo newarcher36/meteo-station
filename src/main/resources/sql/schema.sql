30e11d1be7ff58c498e800e180b2eeb87e512d3f
danijm26 <danijm26@gmail.com>
2022-11-20 08:43:16 +0100

    Fixed docker compose

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA meteostation;
+
+CREATE TABLE IF NOT EXISTS meteostation.meteodata
+(
+    id                     SERIAL PRIMARY KEY,
+    registration_date_time DATE,
+    temperature            FLOAT(2),
+    pressure               FLOAT(2),
+    elevation              FLOAT(2)
+)
